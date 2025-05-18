63ed0ff38d835c2f0de4aa6da1e299111ba4af06
saiveerendranath <40311376+saiveerendranath@users.noreply.github.com>
2025-05-18 17:45:33 -0400

    Create db_script.sql

---
+++ scripts/sql_script/db_script.sql
@@ -0,0 +1,21 @@
+USE master;
+GO
+
+  -- CREATING DATABASE
+CREATE DATABASE DataWarehouse;
+GO
+
+
+  -- CREATING the Schema
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
+
+
+
+
